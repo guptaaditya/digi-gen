f89540c3698b32d05bd4f6b65431a51d3d3d67fa
Aditya Gupta <aditya.gupta@mcmcg.com>
2018-01-21 17:16:05 +0530

    Digital generation test code. Removed some store from from store_directry.json for demo purpose. We can add, as many stores as we want

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import * as mapsClass from './actions/gmap';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      favoriteStores: [],
+      loadingStoresOnMap: false,
+      showLoader: false
+    }
+  }
+
+  componentDidMount() {
+    mapsClass.addGmapListener(this.mapStarter);
+  }
+
+  componentWillUnmount() {
+    mapsClass.removeGmapListener();
+  }
+
+  mapStarter = () => {
+    let intervalLoader = setInterval(() => {this.setState({showLoader: !this.state.showLoader})}, 1000);
+    this.setState({loadingStoresOnMap: true}, () => {
+      mapsClass.initMap(this.markerHandler).then(() => {
+        clearInterval(intervalLoader);
+        this.setState({loadingStoresOnMap: false});
+      }).then(() => {
+        alert('You can click on map markers(Red baloon) to add them to your favorite Red Barn Stores');
+      });
+    });
+  }
+
+  markerHandler = (fav, item) => {
+    let { favoriteStores } = this.state;
+    if(fav) {
+      favoriteStores.push(item)
+    } else {
+      favoriteStores = favoriteStores.filter(i => (i.Name !== item.Name));
+    }
+    this.setState({favoriteStores});
+  }
+
+  render() {
+    return (
+      <div style={{flex: 1, display: 'flex', flexDirection: 'row'}}>
+        <div style={{flex: 1, display: 'flex'}}>
+          {this.state.favoriteStores.length ?
+            <div style={{flex: 10}}>
+              Below is the list of your favorite stores
+              <ul>
+                {this.state.favoriteStores.map((store, index) => <li key={index}>{store.Name}</li>)}
+              </ul>
+            </div>
+          : ''}
+          <div id="map" style={{flex: 30}}></div>
+        </div>
+        {this.state.loadingStoresOnMap &&
+          <div style={{flex: 1, position: 'absolute', zIndex: 99999999, display: 'flex',
+            opacity: '0.7', height: '100%', width: '100%', justifyContent: 'center', backgroundColor: '#ccc'}}>
+            {
+              this.state.showLoader &&
+                <h2>Loading Red Barn Stores from google maps...</h2>
+            }
+          </div>
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/actions/gmap.js
@@ -0,0 +1,87 @@
+import * as storeDetails from '../../store_directory.json';
+
+export function decodeAddress(item, map, google, failedDecodings, markerHandler) {
+  return new Promise(function(res, rej){
+    setTimeout(function(){
+      let geocoder = new google.maps.Geocoder();
+      let googleDefaultIcon = "http://maps.google.com/mapfiles/kml/paddle/red-circle.png";
+      let favoriteStoreIcon = "https://cdn2.iconfinder.com/data/icons/social-buttons-2/512/memori-24.png";
+      geocoder.geocode( { 'address': item.Address}, function(results, status) {
+        if (status === 'OK') {
+          let marker = new google.maps.Marker({
+              map,
+              position: results[0].geometry.location,
+              title: item.Name,
+              icon: googleDefaultIcon
+          });
+          map.setCenter(results[0].geometry.location);
+          google.maps.event.addListener(marker, 'click', function() {
+            if(this.customInfo === 'favorite') {
+              this.customInfo = '';
+              marker.setIcon(googleDefaultIcon);
+            } else {
+              this.customInfo = 'favorite';
+              marker.setIcon(favoriteStoreIcon);
+            }
+            if(markerHandler) {
+              markerHandler(this.customInfo, item);
+            }
+          });
+        } else {
+          item.status = status
+          failedDecodings.push(item);
+        }
+        res();
+      });
+    }, 250);
+  });
+}
+
+export function addGmapListener(listner) {
+  document.addEventListener("gmap-loaded", listner);
+}
+
+export function removeGmapListener(listner) {
+  document.removeEventListener("gmap-loaded", listner);
+}
+
+export function initMap(markerHandler) {
+  let mexico = {lat: 19.4733487, lng: -99.157019};
+  let map = new window.google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: mexico
+  });
+
+  var p = emptyPromise();
+  var failedDecodings = [];
+  storeDetails.default.forEach(item => {
+    p = p.then(() => {
+      return decodeAddress(item, map, window.google, failedDecodings, markerHandler);
+    })
+  });
+  p = p.then(() => {
+    console.log(`Failed geocoding for ${failedDecodings.length} stores`, failedDecodings);
+    //retry for over limit failures
+    var failedRetryDecodings = [];
+    let queryOverLimitFailedDecodings = failedDecodings.filter(i => (i.status === 'OVER_QUERY_LIMIT'));
+    console.log(`Retrying for over usage failures`, queryOverLimitFailedDecodings);
+    var p1 = emptyPromise();
+    queryOverLimitFailedDecodings.forEach(item => {
+      p1 = p1.then(() => {
+        return decodeAddress(item, map, window.google, failedRetryDecodings);
+      });
+    });
+    return p1;
+  });
+  p.then(() => {
+    map.setZoom(11);
+    map.setCenter(mexico);
+  });
+  return p;
+}
+
+export function emptyPromise(){
+  return new Promise(function(resolve, reject){
+    setTimeout(resolve, 0);
+  });
+}
